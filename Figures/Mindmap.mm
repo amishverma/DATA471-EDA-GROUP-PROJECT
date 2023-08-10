964955db7927c7c8908ab4a04ea6fd519c5c4aaa
tramchau <chauthimaitram@gmail.com>
2023-08-10 22:17:28 +1200

    Add files via upload
    Add mindmap files (freeplane and jpg)

---
+++ Figures/Mindmap.mm
@@ -0,0 +1,168 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Can we predict post-covid activity index by pre-covid data?" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1691652568078" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" associatedTemplateLocation="template:/standard-1.6.mm" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="10" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Why" POSITION="bottom_or_right" ID="ID_1754051755" CREATED="1691200139560" MODIFIED="1691200144624">
+<edge COLOR="#ff0000"/>
+<node TEXT="Literature Review" ID="ID_473622910" CREATED="1691200250911" MODIFIED="1691652648121">
+<node TEXT="New Zealand Activity Index (NZAC): Technical note" ID="ID_380748969" CREATED="1691652576588" MODIFIED="1691652601298"/>
+<node TEXT="Background on the Chicago Fed National Activity Index" ID="ID_853309891" CREATED="1691652640483" MODIFIED="1691652647394"/>
+</node>
+<node TEXT="SMART" ID="ID_916724678" CREATED="1691200262874" MODIFIED="1691200271023">
+<node TEXT="Specific" ID="ID_747497776" CREATED="1691200271028" MODIFIED="1691200275054">
+<node TEXT="Economic activity index of New Zealand" ID="ID_1648368130" CREATED="1691652660902" MODIFIED="1691652689501"/>
+<node TEXT="Post-covid and pre-covid" ID="ID_1224056597" CREATED="1691652678314" MODIFIED="1691652705831"/>
+<node TEXT="Economic indicators" ID="ID_1194065355" CREATED="1691652706441" MODIFIED="1691652711324"/>
+</node>
+<node TEXT="Measurable" ID="ID_85147127" CREATED="1691200281965" MODIFIED="1691200290975">
+<node TEXT="Predictable or Correlation" ID="ID_734437506" CREATED="1691652714375" MODIFIED="1691653318747"/>
+<node TEXT="Prediction accuracy" ID="ID_1364092228" CREATED="1691652754703" MODIFIED="1691652761105"/>
+</node>
+<node TEXT="Achievable" ID="ID_451399909" CREATED="1691200292548" MODIFIED="1691653159919">
+<node TEXT="Possibility of correlation or causation relationship" ID="ID_1600396846" CREATED="1691652812144" MODIFIED="1691653222186"/>
+</node>
+<node TEXT="Relevant" ID="ID_1744776809" CREATED="1691200298366" MODIFIED="1691652908014">
+<node TEXT="Monitor economic activities frequently" ID="ID_831612672" CREATED="1691652835479" MODIFIED="1691652907414"/>
+<node TEXT="Uncertainty of the economy during and after Covid" ID="ID_205039767" CREATED="1691652927168" MODIFIED="1691653456067"/>
+</node>
+<node TEXT="Time-bound" ID="ID_544664859" CREATED="1691200301488" MODIFIED="1691200307778">
+<node TEXT="Due date for EDA report is 5th Sep 2023" ID="ID_1894237703" CREATED="1691652958350" MODIFIED="1691656604231"/>
+<node TEXT="4 weeks (including 2 weeks midterm break)" ID="ID_271425770" CREATED="1691653395600" MODIFIED="1691653440315"/>
+</node>
+</node>
+<node TEXT="Benefits" ID="ID_124378001" CREATED="1691200308191" MODIFIED="1691200315348">
+<node TEXT="Data-based evidence relating to economic activities" ID="ID_507286713" CREATED="1691653535048" MODIFIED="1691654313309"/>
+<node TEXT="Relationship of different indicators to the Activity Index" ID="ID_1548099380" CREATED="1691653557325" MODIFIED="1691654300883"/>
+<node TEXT="Form a better idea about NZ economic activities" ID="ID_1186157815" CREATED="1691653583418" MODIFIED="1691653642618"/>
+</node>
+</node>
+<node TEXT="What" POSITION="bottom_or_right" ID="ID_690492507" CREATED="1691200146073" MODIFIED="1691200224544">
+<edge COLOR="#0000ff"/>
+<node TEXT="Assignment 2 - Part 1" ID="ID_138094306" CREATED="1691200319454" MODIFIED="1691200332084"/>
+<node TEXT="EDA report (group)" ID="ID_230467898" CREATED="1691200332557" MODIFIED="1691200340916"/>
+<node TEXT="Individual report" ID="ID_1043220980" CREATED="1691200341520" MODIFIED="1691200345962"/>
+</node>
+<node TEXT="Who" POSITION="bottom_or_right" ID="ID_504336115" CREATED="1691200146306" MODIFIED="1691200157382">
+<edge COLOR="#00ff00"/>
+<node TEXT="Stakeholders" ID="ID_497044718" CREATED="1691200393583" MODIFIED="1691200396917">
+<node TEXT="Richard" ID="ID_1015399185" CREATED="1691200396922" MODIFIED="1691200401505">
+<node TEXT="Supervising time series project" ID="ID_1198511617" CREATED="1691653661339" MODIFIED="1691654438246"/>
+</node>
+<node TEXT="Louise" ID="ID_324388923" CREATED="1691200402121" MODIFIED="1691388017283">
+<node TEXT="Coordinating the course" ID="ID_1150362326" CREATED="1691653671882" MODIFIED="1691654456718"/>
+</node>
+</node>
+<node TEXT="Team" ID="ID_1602377893" CREATED="1691200417835" MODIFIED="1691200420130">
+<node TEXT="Amish" ID="ID_220777202" CREATED="1691200420136" MODIFIED="1691388088763">
+<node TEXT="Git setup &amp; folder tree" ID="ID_609024208" CREATED="1691388053674" MODIFIED="1691388080336"/>
+</node>
+<node TEXT="Izzy" ID="ID_850644168" CREATED="1691200422582" MODIFIED="1691200427214">
+<node TEXT="Economic background &amp; Data" ID="ID_654858684" CREATED="1691388093487" MODIFIED="1691654244163"/>
+</node>
+<node TEXT="Kim" ID="ID_349319823" CREATED="1691200427939" MODIFIED="1691200429040">
+<node TEXT="Project managing (Gantchart)" ID="ID_1637249470" CREATED="1691388196979" MODIFIED="1691653847777"/>
+</node>
+<node TEXT="Tram" ID="ID_1853157475" CREATED="1691200429677" MODIFIED="1691200431482">
+<node TEXT="Documenting (Kanban &amp; Mindmap)" ID="ID_1491268096" CREATED="1691388217534" MODIFIED="1691653822126"/>
+</node>
+</node>
+</node>
+<node TEXT="When, and What with" POSITION="bottom_or_right" ID="ID_593917201" CREATED="1691200148255" MODIFIED="1691200171885">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Resource" ID="ID_172783097" CREATED="1691200437402" MODIFIED="1691200439581">
+<node TEXT="Time" ID="ID_1835108151" CREATED="1691388639593" MODIFIED="1691388658966"/>
+<node TEXT="Tools &amp; Software" ID="ID_1729659739" CREATED="1691388659620" MODIFIED="1691653872308"/>
+<node TEXT="Zoom" ID="ID_1093621497" CREATED="1691388662813" MODIFIED="1691388666007"/>
+</node>
+<node TEXT="Restrictions" ID="ID_1374101795" CREATED="1691200444689" MODIFIED="1691200448057">
+<node TEXT="Everything must be reproducible" FOLDED="true" ID="ID_1664878844" CREATED="1691200454329" MODIFIED="1691656249864">
+<node TEXT="Everything must be reproducible" ID="ID_130155726" CREATED="1691656241095" MODIFIED="1691656241095"/>
+</node>
+<node TEXT="Commit work to GitHub" ID="ID_1781648391" CREATED="1691656262882" MODIFIED="1691656291064"/>
+</node>
+<node TEXT="Timeline" ID="ID_592000479" CREATED="1691656331905" MODIFIED="1691656333867">
+<node TEXT="Week4 - Each member researches time series data" ID="ID_218251549" CREATED="1691656333885" MODIFIED="1691656367597"/>
+<node TEXT="Week5 - Group agree on the data and research question" ID="ID_24347953" CREATED="1691656368168" MODIFIED="1691656397699"/>
+<node TEXT="Week6 - Group conduct EDA" ID="ID_1451581270" CREATED="1691656398472" MODIFIED="1691656492492"/>
+<node TEXT="Two weeks break - Report writing" ID="ID_1328994109" CREATED="1691656527051" MODIFIED="1691656644432"/>
+<node TEXT="Week7 - EDA report submission (5th Sep)" ID="ID_1618751754" CREATED="1691656403694" MODIFIED="1691656597499"/>
+</node>
+</node>
+<node TEXT="How" POSITION="bottom_or_right" ID="ID_1984397094" CREATED="1691200148455" MODIFIED="1691200173469">
+<edge COLOR="#00ffff"/>
+<node TEXT="Risks" ID="ID_968215132" CREATED="1691200452235" MODIFIED="1691200454324">
+<node TEXT="Not applicable" ID="ID_652344003" CREATED="1691654020464" MODIFIED="1691654033443"/>
+</node>
+<node TEXT="Cost" ID="ID_1471287627" CREATED="1691654035036" MODIFIED="1691654036416">
+<node TEXT="Not applicable" ID="ID_1975509782" CREATED="1691654036420" MODIFIED="1691654040090"/>
+</node>
+</node>
+</node>
+</map>
